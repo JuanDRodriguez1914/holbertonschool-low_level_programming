24a128dc888882e37c9f104e825cbaef521ab404
Juanda-creator77 <juandaroviuniver@gmail.com>
2021-02-04 15:37:10 +0000

    SHELL PROYECT 4 por fin

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-int main(void)
-{
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
 
+/**
+ * main - You look good today
+ * Description: Shell proyect metal gear
+ * Return: Programing is like building a multilingual puzzle
+ */
+Int main(void)
+
+{
+	char snake[54] = "\"Programming is like building a multilingual puzzle";
+
+	puts(snake);
+	return (0);
 }
