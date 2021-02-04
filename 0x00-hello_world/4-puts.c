d2078e8ab877e11e94a8c0910f3341429c9aa3d9
Juanda-creator77 <juandaroviuniver@gmail.com>
2021-02-04 13:41:53 +0000

    SHELL PROYECT

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main(void)
 {
-        char stri[45]="\"Programming is like building a multilingual";
+        char stri[45]="\"Programming is like building a multilingual puzzle";
         puts(stri);
         return (0);
 }
